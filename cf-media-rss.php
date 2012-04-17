3c96f5769283023e9736e56060cf354d53c03dc2
Evan Anderson <ejdanderson@gmail.com>
2012-04-17 10:31:42 -0600

    Only get attachments which have an image mime type, check for array and not empty

--- cf-media-rss.php
+++ cf-media-rss.php
@@ -12,25 +12,29 @@ function cf_media_rss_links() {
 	global $post;
 	$items = get_children(
 		array(
-			'post_parent' => $post->ID
+			'post_parent' => $post->ID,
+			'post_type' => 'attachment',
+			'post_mime_type' => 'image',
 		)
 	);
-	foreach ($items as $item) {
-		$info = wp_get_attachment_metadata($item->ID);
-		$thumb = wp_get_attachment_image_src($item->ID);
-		echo '
-<media:content 
-	url="'.wp_get_attachment_url($item->ID).'"
-	type="'.$item->post_mime_type.'"
-	width="'.$info['width'].'"
-	height="'.$info['height'].'"
-/>
-<media:thumbnail 
-	url="'.$thumb[0].'" 
-	width="'.$thumb[1].'" 
-	height="'.$thumb[2].'" 
-/>
-		';
+	if (is_array($items)) {
+		foreach ($items as $item) {
+			$info = wp_get_attachment_metadata($item->ID);
+			$thumb = wp_get_attachment_image_src($item->ID);
+			echo '
+	<media:content 
+		url="'.wp_get_attachment_url($item->ID).'"
+		type="'.$item->post_mime_type.'"
+		width="'.$info['width'].'"
+		height="'.$info['height'].'"
+	/>
+	<media:thumbnail 
+		url="'.$thumb[0].'" 
+		width="'.$thumb[1].'" 
+		height="'.$thumb[2].'" 
+	/>
+			';
+		}
 	}
 }
 add_action('rss2_item', 'cf_media_rss_links');
